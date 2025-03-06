d73d99bb5d02c9e82f63b0a8a9e97bf2ef07e2b5
kvnbanunu <kvnbanunu@gmail.com>
2025-03-06 13:56:13 -0800

    ready for pages

--- backend/modules/openai.js
+++
@@ -1,6 +0,0 @@
-import OpenAI from 'openai';
-require('dotenv').config();
-
-export const openai = new OpenAI({
-    apiKey: process.env.OPEN_AI_KEY,
-});
--- backend/modules/paths.js
+++
@@ -1,5 +0,0 @@
-export const paths = {
-    prompt:'/prompt',
-    signup:'/signup',
-    login:'/login'
-};
