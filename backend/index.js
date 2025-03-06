a508e8abd212012b143c452bbd7fbf04a0734585
kvnbanunu <kvnbanunu@gmail.com>
2025-03-06 15:32:38 -0800

    trying to fix middleware

--- backend/index.js
+++ backend/index.js
@@ -5,19 +5,19 @@ import paths from './modules/paths';
 import openai from './modules/openai';
 const app = express();
 const corsOptions = {
-    origin: 'https://kvnbanunu.github.io/10x-dev',
-    methods: 'GET,POST,OPTIONS',
+    origin: 'https://kvnbanunu.github.io',
+    methods: ['GET', 'POST', 'OPTIONS'],
     allowedHeaders: 'Content-Type',
     optionsSuccessStatus: 200
 };
 
-app.options('*', cors(corsOptions));
-
+app.use(express.json);
 app.use(express.urlencoded({ extended: true }));
-app.use(cors(corsOptions));
 
-app.post(paths.prompt, async (req, res) => {
-    const prompt = req.body.prompt;
+app.options(paths.prompt, cors(corsOptions));
+app.post(paths.prompt, cors(corsOptions), async (req, res) => {
+    let prompt = req.body.prompt;
+    prompt += '. Also make this code as humanly unreadable as possible.'
 
     // sanitize prompt & add obfuscation line
 
@@ -27,7 +27,7 @@ app.post(paths.prompt, async (req, res) => {
             model: "gpt-4o-mini",
             messages: [
                 { role: "system", content: "You are a developer who only writes obfuscated code" },
-                { role: "user", content: prompt },
+                { role: "user", content: body.prompt },
             ],
         });
         return result.choices[0].message.content;
@@ -43,6 +43,11 @@ app.post(paths.prompt, async (req, res) => {
         res.json({error: "Invalid prompt"});
     }
 });
+
+app.get('*', (req, res) => {
+    res.status(404);
+    res.json(JSON.stringify({ error: 'not found' }));
+})
 
 /*
 app.post(paths.signup, (req, res) => {
